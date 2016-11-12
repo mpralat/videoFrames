2fc66ce3fc4791fc1e4743d3ca3b91a07657ea5d
Marta Prałat <pralatmarta@gmail.com>
2016-11-12 20:05:57 +0100

    Iterating through directories done.

--- main.cpp
+++ main.cpp
@@ -2,19 +2,39 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "opencv2/opencv.hpp"
 #include <boost/algorithm/string.hpp>
+#include <glob.h>
+
 #define BOOST_NO_CXX11_SCOPED_ENUMS
+
 #include <boost/filesystem.hpp>
+
 #undef BOOST_NO_CXX11_SCOPED_ENUMS
 
-std::string output_dir = "/home/marta/Inne/Sponge/frames/";
+//std::string output_dir = "/home/marta/Inne/Sponge/frames/";
+std::string main_directory = "/home/marta/Inne/videos/";
 
-using namespace boost::filesystem;
+namespace fs = boost::filesystem;
 
-void saveFrames(cv::VideoCapture capture, std::string mainName) {
+std::string checkDirectory(std::string output_dir) {
+    std::vector<std::string> strings;
+    boost::split(strings, output_dir, boost::is_any_of("/"));
+    if (fs::create_directory(fs::path(output_dir)))
+        std::cerr << "Directory created! " << output_dir << std::endl;
+    std::cout << strings[strings.size() - 3] << std::endl;
+    return strings[strings.size() - 3];
+}
+
+void saveFrames(std::string fileName, std::string outputDir) {
+
     cv::Mat frame;
+    cv::VideoCapture capture(fileName);
+    if (!capture.isOpened())
+        _exit(-1);
+
+    std::string mainName = checkDirectory(outputDir);
+//    Getting a frame each two seconds (it's roughly 24fps).
     double frames = capture.get(CV_CAP_PROP_FRAME_COUNT);
-    for (int i = 24; i < frames - 48; i += 240) {
-        std::cout << i << std::endl;
+    for (int i = 48; i < frames - 48; i += 240) {
 //        Setting the frame pointer to each 240th frame, starting with the first one.
         capture.set(CV_CAP_PROP_POS_FRAMES, i);
         bool success = capture.read(frame);
@@ -22,29 +42,34 @@ void saveFrames(cv::VideoCapture capture, std::string mainName) {
             std::cout << "A wild problem with reading the frame appeared!" << std::endl;
             break;
         }
-        std::cout << "nothing wrong, proceeding" << std::endl;
-        cv::imwrite(output_dir + mainName + "_" + std::to_string(i) + ".jpg", frame);
+        cv::imwrite(outputDir + mainName + "_" + fileName + "_" + std::to_string(i) + ".jpg", frame);
+        frame.release();
     }
+    capture.release();
 }
-
-std::string checkDirectory(std::string output_dir) {
-    std::vector<std::string> strings;
-    boost::split(strings, output_dir, boost::is_any_of("/"));
-    if (create_directory(path(output_dir)))
-        std::cerr << "Directory created! " << output_dir << std::endl;
-    return strings[strings.size() - 3];
-}
-
 
 int main() {
-    std::string videoName = "/home/marta/Inne/Sponge/ep1_C.avi";
 
-    cv::VideoCapture capture(videoName);
-    if (!capture.isOpened())
-        return -1;
-    std::cout << "No errors!" << std::endl;
+    fs::path input(main_directory);
+//    Iterating through directories.
+    for (auto i = fs::directory_iterator(input); i != fs::directory_iterator(); i++) {
+        if (is_directory(i->path())) {
+            fs::current_path(i->path());
+            glob_t globbuf;
+//            Getting all avi files from the dir.
+            int err = glob("*.avi", 0, NULL, &globbuf);
+            if (err == 0)
+                for (size_t j = 0; j < globbuf.gl_pathc; j++) {
+                    std::cout << globbuf.gl_pathv[j] << std::endl;
+                    std::string outputDir = i->path().string() + "/frames/";
+                    saveFrames(globbuf.gl_pathv[j], outputDir);
+                }
 
-    saveFrames(capture, checkDirectory(output_dir));
+            globfree(&globbuf);
+
+        } else
+            continue;
+    }
 
     return 0;
 }
